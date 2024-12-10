ab3d9be81e6a2de37de3c5eaf0dd8f9e4240398b
Bas de Bruin <bassie00001@outlook.com>
2024-12-10 13:34:23 +0100

    Created project2

---
+++ authentication-service/src/test/java/nl/kwetter2/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.kwetter2.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
